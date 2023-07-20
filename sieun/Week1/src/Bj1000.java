0a23dbc45e7946a7122d62bf54deb2e94a12cd60
sieun <21800405@handong.ac.kr>
2023-07-20 22:17:27 +0900

    Problem Solved(2557, 1000)

---
+++ sieun/Week1/src/Bj1000.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+public class Bj1000 {
+	public static void main(String[] args) {
+		int A, B;
+		Scanner sc = new Scanner(System.in);
+		
+		A = sc.nextInt();
+		B = sc.nextInt();
+		
+		System.out.println(A+B);
+	}
+}
