314ae1cccc46078ecd61c7eac267d738d61198c9
김주이 <juyi077@naver.com>
2023-10-14 18:35:45 +0900

    solved(Bj1912)

--- juyi/Week11/src/Bj1912.java
+++ juyi/Week11/src/Bj1912.java
@@ -1,14 +1,37 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
-/*
- * 시간초과
- */
+
 public class Bj1912 {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = Integer.parseInt(br.readLine());
+
+		int[] num = new int[n + 1];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 1; i < n + 1; i++) {
+			num[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int[] dp = new int[n + 1];
+		dp[1] = num[1];
+		
+		int max = dp[1];
+		for(int i = 2; i < n + 1; i++) {
+			dp[i] = Math.max(dp[i-1] + num[i], num[i]);	// 이전까지의 합 + 현재 값과 현재 값 중 큰 값을 dp에 저장
+			max = Math.max(max, dp[i]);	// 최대값 찾기
+		}
+		
+		System.out.println(max);
+		
+	}
+	
+	// 시간초과
+	/*public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		int n = Integer.parseInt(br.readLine());
@@ -39,6 +62,6 @@ public class Bj1912 {
 		
 		System.out.println(max);
 
-	}
+	}*/
 
 }
